b1bb420a43c08a1cc05c65024951b0a6c6911d16
GusnaRWX <dadangusna777@gmail.com>
2021-01-17 16:38:57 +0700

    menambahkan list pada index.php

--- index.php
+++ index.php
@@ -10,5 +10,9 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis unde ducimus veniam iure,
      cupiditate similique quibusdam commodi vel placeat ut ad officiis! Quidem, totam molestiae optio
       temporibus pariatur architecto porro!</p>
+      <ul>
+      <li>1</li>
+      <li>2</li>
+      </ul>
 </body>
 </html>
